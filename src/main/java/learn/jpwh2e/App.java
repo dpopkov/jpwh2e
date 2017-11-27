706b9cbf4f76f886866d686e1b39ef82f1151b11
dpopkov <pkvdenis@gmail.com>
2017-11-27 14:50:01 +0300

    Project structure

---
+++ src/main/java/learn/jpwh2e/App.java
@@ -0,0 +1,10 @@
+package learn.jpwh2e;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/learn/jpwh2e/AppTest.java
@@ -0,0 +1,14 @@
+package learn.jpwh2e;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @org.junit.Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
